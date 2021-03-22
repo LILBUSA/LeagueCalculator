dc54611e18415cec7c96f4d6d6d6f0ec19502f31
tblade <bigtay312@gmail.com>
2021-03-22 09:50:26 -0400

    idek this was from last time

--- src/TokenCalculator.java
+++ src/TokenCalculator.java
@@ -111,11 +111,18 @@ public class TokenCalculator implements Calculator {
 //
 //    }
 
-    public Champion pickTop(Stats stats) {
-        ArrayList<Champion> top = stats.getTop();
-        for (int i = 0; i < stats.getTop().size(); i++) {
-
-        }
-    }
+//    public Champion pickTop(Stats stats) {
+//        ArrayList<Champion> top = stats.getTop();
+//        for (int i = 0; i < top.size(); i++) {
+//            for (int j = 0; j < top.size() - i - 1; j++) {
+//                if (top.get(j).getPickRate() > top.get(j + 1).getPickRate()) {
+//                    Champion champ = top.get(j);
+//                    top.set(j, top.get(j + 1));
+//                    top.set((j + 1), champ);
+//                }
+//            }
+//        }
+//
+//    }
 }
 
